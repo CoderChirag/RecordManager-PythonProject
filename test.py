ddbb869fd7cd8c274b0dad52b89fe25946f2d802
Chirag Jain <72244964+CoderChirag@users.noreply.github.com>
2020-11-03 21:01:36 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('student.json', 'r') as f:
-    # Getting data from the student file
-    json_data = json.load(f)
-    print(type(json_data))
-    # Pasrsing the json data to create a dictionary
-    # data_dict = json.loads(json_data)
-    print("old data found")
